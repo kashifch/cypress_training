bf0a8cf3c5d5b82f5ce6938fa0d51838a1043daa
Kashif Chaudhry <kashif.chaudhry@arbisoft.com>
2019-12-13 16:39:34 +0500

    lecture1

---
+++ cypress/integration/lecture1/login.spec.js
@@ -0,0 +1,22 @@
+describe('Login and Registration tests', () => {
+    beforeEach(()=> {
+        cy.visit('/')
+    })
+    it('lets the user login to the application', () => {
+        cy.get('.js-user-cta>a[href$="login"]').click()
+        cy.get('input[id="login-email"]').type('temp_user@yopmail.com')
+        cy.get('#login-password').type('edxedxedx1')
+        cy.get('button[type="submit"]').click()
+        cy.title().should('contain', 'Dashboard')
+    })
+
+    it('lets a new user register', ()=> {
+        cy.get('.js-user-cta>a[href$="register"]').click()
+        cy.get('#register-email').type('temp_user11@yopmail.com')
+        cy.get('#register-name').type('Temp User')
+        cy.get('#register-username').type('TempUser11')
+        cy.get('#register-password').type('edxedxedx1')
+        cy.get('#register-country').select('Pakistan')
+        cy.get('button[type="submit"]').click()
+    })
+})
